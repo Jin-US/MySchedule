fd8769624b5e9f028ad70aceec65ed2c057e67d2
leejinus <leejinus@gmail.com>
2020-04-06 19:08:52 +0900

    Initial commit

---
+++ src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lj.myschedule
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val registerButton = findViewById<View>(R.id.registerButton) as TextView
+        registerButton.setOnClickListener {
+            val registerIntent = Intent(this@LoginActivity, Register_activity::class.java)
+            this@LoginActivity.startActivity(registerIntent)
+        }
+        val loginButton = findViewById<View>(R.id.loginButton) as TextView
+        loginButton.setOnClickListener {
+            //                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+//                LoginActivity.this.startActivity(mainIntent);
+        }
+        val NonLogin = findViewById<View>(R.id.Next) as TextView
+        NonLogin.setOnClickListener {
+            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+            this@LoginActivity.startActivity(mainIntent)
+        }
+    }
+}
